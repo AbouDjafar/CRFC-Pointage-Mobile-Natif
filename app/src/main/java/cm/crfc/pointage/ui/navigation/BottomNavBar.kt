afc1b9e326c009d238eaf654c391666251645b48
AbouDjafar <abdoulramaneabou@gmail.com>
2026-05-17 16:17:56 +0100

    Fix bottom nav weight scope

--- app/src/main/java/cm/crfc/pointage/ui/navigation/BottomNavBar.kt
+++ app/src/main/java/cm/crfc/pointage/ui/navigation/BottomNavBar.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
@@ -76,7 +77,7 @@ fun CrfcBottomBar(
 }
 
 @Composable
-private fun BottomNavItem(
+private fun RowScope.BottomNavItem(
     destination: BottomDestination,
     selected: Boolean,
     onClick: () -> Unit
